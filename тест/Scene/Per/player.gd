0d0fca75f3e0474aa4725ef9169c96649e7495f4
Gneg009 <damocl.673@gmail.com>
2026-03-01 20:48:31 +0700

    Меню паузы (Часть 1)
    Сделал скелет меню паузы так сказать. Кнопки и ярлык с Vbox. Скрипта пока нет.

--- тест/Scene/Per/player.gd
+++ тест/Scene/Per/player.gd
@@ -1,68 +1,49 @@
 extends CharacterBody2D
 
-enum {
-	DOWN,
-	UP,
-	LEFT,
-	RIGHT
-}
+enum {DOWN, UP, LEFT, RIGHT}
 
 @onready var anim = $AnimatedSprite2D 
 var speed = 200
 var idle_dir = DOWN
+var input_dir = Vector2.ZERO
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("up"):
-		up_move()
-	elif Input.is_action_pressed("down"):
-		down_move()
-	elif Input.is_action_pressed("left"):
-		left_move()
-	elif Input.is_action_pressed("right"):
-		right_move()
+func _physics_process(_delta: float) -> void:
+	input_dir = Input.get_vector("left", "right", "up", "down")
+	
+	if input_dir != Vector2.ZERO:
+		velocity = input_dir * speed
+		update_animation(input_dir)
 	else:
+		velocity = Vector2.ZERO
 		idle()
-		
+	
 	move_and_slide()
 
-func up_move():
-	anim.play("Up")
-	velocity.x = 0
-	velocity.y = -speed
-	idle_dir = UP
-
-func down_move():
-	anim.play("Down")
-	velocity.x = 0
-	velocity.y = speed
-	idle_dir = DOWN
-
-func left_move():
-	anim.flip_h = true
-	anim.play("Front")
-	velocity.x = -speed
-	velocity.y = 0
-	idle_dir = LEFT
-
-func right_move():
-	anim.flip_h = false
-	anim.play("Front")
-	velocity.x = speed
-	velocity.y = 0
-	idle_dir = RIGHT
+func update_animation(dir: Vector2):
+	if dir.x > 0:
+		anim.flip_h = false
+		anim.play("Front")
+		idle_dir = RIGHT
+	elif dir.x < 0:
+		anim.flip_h = true
+		anim.play("Front")
+		idle_dir = LEFT
+	elif dir.y < 0:
+		anim.play("Up")
+		idle_dir = UP
+	elif dir.y > 0:
+		anim.play("Down")
+		idle_dir = DOWN
 
 func idle():
-	velocity.x = 0
-	velocity.y = 0
-	if velocity.x == 0 and velocity.y == 0:
-		match idle_dir:
-			DOWN:
-				anim.play("Idle_down")
-			UP:
-				anim.play("Idle_up")
-			LEFT:
-				anim.flip_h = true
-				anim.play("Idle_front")
-			RIGHT:
-				anim.flip_h = false
-				anim.play("Idle_front")
+	match idle_dir:
+		DOWN: 
+			anim.play("Idle_down")
+		UP: 
+			anim.play("Idle_up")
+		LEFT:
+			anim.flip_h = true
+			anim.play("Idle_front")
+		RIGHT:
+			anim.flip_h = false
+			anim.play("Idle_front")
